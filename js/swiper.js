fdf981bd0280c3bfaa3dbb495b5d882c3752ec05
Yusuf Azzam <yusuf4zzam@gmail.com>
2024-12-15 12:11:49 +0200

    Pushing The Intial Files

---
+++ js/swiper.js
@@ -0,0 +1,24 @@
+// App Showcase Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+});
+
